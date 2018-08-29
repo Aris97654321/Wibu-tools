8191f140bfc16a51d7f127bca080b22f556bbd30
ariisabdlzz <Arisabdulaziz23@gmail.com>
2018-08-29 17:00:42 +0700

    Update and rename yyy.sh to menu.sh

--- menu.sh
+++ menu.sh
@@ -65,11 +65,11 @@ read -p "Tulis Pilihan Anda(Angka)= " bro
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
-echo "\033[34;1m"
-figlet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
+cd Wibu-tools
+cd tools
 php tp.php
 fi
 
@@ -77,137 +77,140 @@ if
 [ $bro = 2 ] || [ $bro = 2 ]
 then
 clear
-echo "\033[31;1m"
-toilet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
+cd Wibu-tools
+cd tools
 php t.php
 fi
 
 if [ $bro = 3 ] || [ $bro = 3 ]
 then
-clear
-echo "\033[31;1m"
-figlet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
+cd Wibu-tools
+cd tools
 php mataharimall.php
 fi
 
 if [ $bro = 4 ] || [ $bro = 4 ]
 then
 clear
-echo "\033[33;1m"
-toilet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
+cd Wibu-tools
+cd tools
 php hooq.php
 fi
 
 if [ $bro = 5 ] || [ $bro = 5 ]
 then
 clear
-toilet -f slant --gay "Mr.B4J1N64N"
-echo "\033[31;1m"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
+cd Wibu-tools
+cd tools
 php kfc.php
 fi
 
 if [ $bro = 6 ] || [ $bro = 6 ]
 then
 clear
-toilet -f mono12 -F gay "Mr.B4J1N64N"
-echo "\033[30;1m"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
+cd Wibu-tools
+cd tools
 php phd.php
 fi
 
 if [ $bro = 7 ] || [ $bro = 7 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
+cd Wibu-tools
+cd tools
 php whiskas.php
 fi
 
 if [ $bro = 8 ] || [ $bro = 8 ]
 then
 clear
-figlet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
+cd Wibu-tools
+cd tools
 php zipay.php
 fi
 
 if [ $bro = 9 ] || [ $bro = 9 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
-php key.php
+cd Wibu-tools
+cd tools
+python2 SpamMail.py
 fi
 
 if [ $bro = 10 ] || [ $bro = 10 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
-python2 SpamMail.py
+cd Wibu-tools
+cd fb-bot
+python2 osif.py
+
 fi
 
 if [ $bro = 11 ] || [ $bro = 11 ]
 then
 clear
-toilet "Mr.B4J1N64N"
-cd
-cd T00Ls-B4J1N64N2
-cd module
-python2 whs.py
+echo ""
+figlet "WIBU-404"|lolcat
+echo "Script belum bisa digunakan" |lolcat
+echo "Masih tahap uji pembuatan" | lolcat
 fi
 
 if [ $bro = 12 ] || [ $bro = 12 ]
 then
 clear
-toilet "Mr.B4J1N64N"
-cd
-cd T00Ls-B4J1N64N2
-cd module
-python2 upf.py
+echo ""
+figlet "WIBU-404"|lolcat
+echo "Anda harus mengedit script ini lagi" |lolcat
+echo "Cebong mana tau ea....." |lolcat
 fi
 
 if [ $bro = 13 ] || [ $bro = 13 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-cd module
-python2 create.py
+cd Wibu-tools
+cd tools
+php key.php
 fi
 
 if [ $bro = 14 ] || [ $bro = 14 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+echo ""
+figlet "WIBU-404"|lolcat
 cd
-cd T00Ls-B4J1N64N2
-ls
-cd module
-python2 dog.py
+cd Wibu-tools
+cd tools
+python2 whs.py
 fi
 
 if [ $bro = 15 ] || [ $bro = 15 ]
